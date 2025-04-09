7401f4ca7a4e41baf70f314db991c775391e9469
LeandroAS27 <leandro2791@hotmail.com>
2025-04-09 17:59:44 -0300

    feito o formulario para criacao de produtos

--- ecommerce-frontend/src/app/profile/page.jsx
+++ ecommerce-frontend/src/app/profile/page.jsx
@@ -6,13 +6,11 @@ import Footer from "../components/footer";
 
 //redux
 import { useDispatch, useSelector } from "react-redux";
-import { setProducts } from "../redux/productSlice";
 import { toggleCart } from "../redux/modalSlice";
 
 //react
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 //images
 import noImage from '../../../public/image-no-image.jpg';
@@ -23,9 +21,7 @@ import information from '../../../public/icons8-informações-96.png';
 
 const Profile = () => {
     const dispatch = useDispatch();
-    const products = useSelector(state => state.products.items);
     const isModalOpen = useSelector(state => state.modalCart.isModalOpen);
-    const router = useRouter();
     const [myOrderDetails, setMyOrderDetails] = useState([])
     const [userActive, setuserActive] = useState(null)
     const [isAdmin, setIsAdmin] = useState(false)
@@ -53,15 +49,14 @@ const Profile = () => {
         dispatch(toggleCart())
     }
 
-    const handleMyProfileDetails = () => {
+    const handleMyProfileDetails = (data) => {
         const userData = JSON.parse(localStorage.getItem('user'))
         setuserActive(userData)
-        setActiveView('profile')
+        setActiveView(data)
     }
 
 
     const handleMyOrderDetails = async() => {
-        console.log('funcionou')
         try {
             const response = await fetch('http://localhost:5000/order')
             if(!response.ok){
@@ -75,7 +70,6 @@ const Profile = () => {
         }
     }
 
-    console.log(myOrderDetails)
 
     return(
         <>
@@ -85,12 +79,14 @@ const Profile = () => {
                     <ModalCart isOpen={isModalOpen} onClose={handleCartModal}/>
                 </header>
 
-                <main className="w-full h-dvh flex">    
-                    <div className="w-full flex ">
-                        <div className="w-full flex">
-                            <nav className="w-1/3 border-r p-4">
+                <main className="w-full h-dvh flex mb-4">    
+                    <div className="w-full flex">
+                        <div className="w-full flex flex-col md:flex-row">
+                            <nav className="w-full md:w-1/3 md:border-r p-4">
                                 <ul className="space-y-4">
-                                    <li className="flex space-x-2 border-b items-center p-2 cursor-pointer" onClick={handleMyProfileDetails}>
+                                    <li className={`flex space-x-2 border-b items-center p-2 cursor-pointer rounded ${activeView === 'profile' ? "bg-gray-200 transition duration-300" : "bg-transparent"}`} 
+                                    onClick={() => handleMyProfileDetails('profile')}
+                                    >
                                         <Image 
                                         src={person} 
                                         alt="icone de uma pessoa" 
@@ -101,7 +97,9 @@ const Profile = () => {
                                         <span>Meu cadastro</span>
                                     </li>
 
-                                    <li className="flex space-x-2 border-b items-center p-2 cursor-pointer" onClick={handleMyOrderDetails}>
+                                    <li className={`flex space-x-2 border-b items-center p-2 cursor-pointer rounded ${activeView === 'orders' ? "bg-gray-200 transition duration-300" : "bg-transparent"}`} 
+                                    onClick={handleMyOrderDetails}
+                                    >
                                         <Image 
                                         src={box} 
                                         alt="icone de uma pessoa" 
@@ -113,7 +111,9 @@ const Profile = () => {
                                     </li>
 
                                     {isAdmin && (
-                                        <li className="flex space-x-2 border-b items-center p-2 cursor-pointer">
+                                        <li className={`flex space-x-2 border-b items-center p-2 cursor-pointer rounded ${activeView === 'createProduct' ? "bg-gray-200 transition duration-300" : "bg-transparent"}`} 
+                                        onClick={() => handleMyProfileDetails('createProduct')}
+                                        >
                                             <Image
                                             src={noImage}
                                             alt="icone para criar pedidos"
@@ -129,15 +129,15 @@ const Profile = () => {
 
                             {/* area de conteudo dinamico */}
                             {activeView === 'profile' && userActive ? (
-                                <section className="w-2/3 p-6">
-                                    <h2 className="text-xl mb-8">Dados do usuário</h2>
+                                <section className="w-full md:w-2/3 p-6">
+                                    <h2 className="text-xl font-semibold mb-8">Dados do usuário</h2>
                                     <div className="w-full rounded-lg shadow-md border flex flex-col p-6">
                                         <p>Nome: {userActive.user.name}</p>
                                         <p>Email: {userActive.user.email}</p>
                                     </div>
                                 </section>
                             ) : activeView === 'orders' && myOrderDetails && myOrderDetails.result ? (
-                                <section className="w-2/3 p-6">
+                                <section className="w-full md:w-2/3 p-6">
                                     <h2 className="text-xl font-semibold mb-4">Detalhes do pedido</h2>
                                     <table className="w-full border border-gray-300 text-left">
                                         <thead className="bg-gray-100">
@@ -160,7 +160,71 @@ const Profile = () => {
                                         </tbody>
                                     </table>
                                 </section>
-                            ) : ( 
+                            ) : activeView === 'createProduct' && userActive ? (
+                                // formulario para criar produto (adicionar imask)
+                                <section className="w-full md:w-2/3 p-6 flex flex-col items-center">
+                                    <h1 className="text-xl font-semibold mb-4">Formulario</h1>
+                                    <form className="space-y-4 w-full md:max-w-2/4">
+                                        <div>
+                                            <input 
+                                            type="text" 
+                                            placeholder="Nome do produto" 
+                                            className="border rounded-lg w-full px-4 py-3 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                                            />
+                                        </div>
+                                        <div>
+                                            <textarea 
+                                            type="text"
+                                            placeholder="Descricao do produto"
+                                            className="border rounded-lg w-full px-4 py-3 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                                            />
+                                        </div>
+                                        <div>
+                                            <input 
+                                            type="number" 
+                                            placeholder="Preço"
+                                            className="border rounded-lg w-full px-4 py-3 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                                            />
+
+                                        </div>
+
+                                        <div>
+                                            <select className="bg-white rounded-md p-2 border">
+                                                <option value="">Selecione a categoria do produto</option>
+                                                <option value="1">Feminino</option>
+                                                <option value="2">Infantil</option>
+                                                <option value="3">Masculino</option>
+                                                <option value="4">Coleções</option>
+                                            </select>
+                                        </div>
+
+                                        <div>
+                                            <input 
+                                            type="number" 
+                                            placeholder="Estoque"
+                                            className="border rounded-lg w-full px-4 py-3 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                                            />
+                                        </div>
+
+                                        <div className="relative w-fit">
+                                            <input 
+                                            type="file" 
+                                            id="fileUpload"
+                                            className="hidden"
+                                            />
+                                            <label 
+                                            htmlFor="fileUpload"
+                                            className="bg-gray-300 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md cursor-pointer transtion duration-300"
+                                            >
+                                                Selecionar Imagem
+                                            </label>
+                                        </div>
+                                            <button className="w-full py-3 bg-[#7886C7] cursor-pointer text-white font-semibold rounded-lg hover:bg-[#2D336B] transition">
+                                                Adicionar Produto
+                                            </button>
+                                    </form>
+                                </section> 
+                            ) : (
                                 <div className="w-full h-full flex justify-center items-center">
                                     <div className="flex flex-col justify-center items-center">
                                         <Image 
@@ -168,9 +232,8 @@ const Profile = () => {
                                         alt="Icone de informacao" 
                                         height={96} 
                                         width={96}
-                                        className=""
                                         />
-                                        <p className="text-lg text-black">Clique nos icones a esquerda para ver informações do seu perfil ou pedido</p>
+                                        <p className="text-md md:text-lg text-black text-center">Clique nos icones a esquerda para ver informações do seu perfil ou pedido</p>
                                     </div>
                                 </div>
                             )}
